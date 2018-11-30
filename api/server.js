cc85cd80929f820f28a3452ade62044198d5bef4
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2018-11-29 22:55:18 -0200

    Added comments for API sections

--- api/server.js
+++ api/server.js
@@ -36,6 +36,7 @@ const TEAMS = [
 
 const logged = true;
 
+// User auth verification
 server.use((req, res, next) => {
   console.log('User URL:', req.url);
 
@@ -46,10 +47,12 @@ server.use((req, res, next) => {
   }
 })
 
+// Home endpoints
 server.get('/', (request, response) => {
   response.send('<h1>Home</h1>');
 })
 
+// Teams endpoints
 server.get('/teams', (req, res) => {
   res.send(TEAMS);
 })
@@ -63,14 +66,17 @@ server.get('/tournaments', (request, response) => {
   response.send('<h1>Tournaments</h1>');
 })
 
+// Players endpoints
 server.get('/players', (request, response) => {
   response.send('<h1>Players</h1>');
 })
 
+// Countries endpoints
 server.get('/countries', (request, response) => {
   response.send('<h1>Countries</h1>');
 })
 
+// Not Found endpoints
 server.get('/notfound', (request, response) => {
   response.send('<h1>Page Not Found</h1>');
 })
@@ -79,7 +85,7 @@ server.use((req, res, next) => {
   res.send({msg:'Path not found. Try another option :)'});
 });
 
-
+// Status for user on terminal 
 server.listen(port, () => {
   console.log(`The server is running on: http://localhost:${port}`);
   console.log('Press Ctrl-C to shutdown this server');
