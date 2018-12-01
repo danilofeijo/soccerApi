5e2e349f48506363500d6f8ee4416ef96937341b
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2018-12-01 13:04:49 -0200

    Add server configurations

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const app = express();
+const port = 8081;
+
+app.use(bodyParser.json());
+
+// Status for user on terminal 
+app.listen(port, () => {
+  console.log(`Server running on: http://localhost:${port}`);
+  console.log('Ctrl-C ==> Shutdown server');
+  console.log('$ node server.js ==> Start the server again');
+})
