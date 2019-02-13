75fbfdcc84ab5c0047bf2eafe38ce05f5f9aa3f5
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2019-02-13 17:09:06 -0200

    Apply express route

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const ENDPOINTS = require('../data/endpointsAvailable.json');
-const TEAMS = require('../data/teams.json');
+
+const teams = require('./teams');
 
 const app = express();
 app.use(bodyParser.json());
-
 const port = 8081;
 const serverUrl = `http://localhost:${port}`;
 
@@ -15,34 +15,7 @@ app.get('/', (req, res) => {
   res.send(ENDPOINTS);
 });
 
-// Teams endpoints
-app.get('/teams', (req, res) => {
-  const reqTeamId = req.query.id;
-  const teamSearchResult = TEAMS.teams[reqTeamId]
-
-  if (!req.query.id) {
-    res.send(TEAMS);
-  } else {
-    res.send(teamSearchResult);
-  }
-
-  // adicionar validação para quando não encontrar um resultado
-
-});
-
-app.post('/teams', (req, res) => {
-  const newTeam = req.body;
-
-  TEAMS.push({
-    id: TEAMS.length + 1,
-    ...newTeam
-  });
-
-  res.send({
-    addedTeam: newTeam,
-    teamsList: TEAMS
-  });
-});
+app.use('/teams', teams);
 
 // Not Found endpoints
 app.get('/notfound', (req, res) => {
