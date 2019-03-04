995420354e4a91fdb8f7d6b299231912b90a7a33
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2019-03-04 18:45:53 -0300

    Set the team endpoint structure

---
+++ src/models/team.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// team schema definition
+const TeamSchema = new Schema(
+	{
+		name: { type: String, required: true },
+		country: { type: String, required: true },
+		foundationDate: { type: Date, default: Date.now },
+		venueStadium: { type: String },
+		venueCapacity: { type: Number }
+	},
+	{
+		versionKey: false
+	}
+);
+
+// Sets the foundationDate parameter equal to the current time
+TeamSchema.pre('save', next => {
+	now = new Date();
+	if (!this.foundationDate) {
+		this.foundationDate = now;
+	}
+	next();
+});
+
+// Exports the TeamSchema for use elsewhere
+module.exports = mongoose.model('team', TeamSchema);
